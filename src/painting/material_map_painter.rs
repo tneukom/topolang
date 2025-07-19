1dab9a500a35f16a1bb63a2c5f15dc5e3379faf7
Tobias Neukom <tneukom@gmail.com>
2025-07-20 01:52:55 +0200

    MaterialMapPainter texture size never decreasing
    We only increase the size of the texture, even if the RgbaField smaller than
    the texture. This way we rarely have to recreate the texture.

--- src/painting/material_map_painter.rs
+++ src/painting/material_map_painter.rs
@@ -15,7 +15,7 @@ pub struct RgbaFieldPainter {
     texcoord_vertices: RectVertices,
     vao: GlVertexArrayObject,
     shader: Shader,
-    texture: Option<(GlTexture, Point<i64>)>,
+    texture: GlTexture,
 }
 
 impl RgbaFieldPainter {
@@ -35,13 +35,33 @@ impl RgbaFieldPainter {
 
         texcoord_vertices.assign_attribute(gl, &shader, "in_bitmap_uv");
 
+        let texture = Self::transparent_texture(gl, Point(256, 256));
+
         Self {
             shader,
-            texture: None,
+            texture,
             position_vertices,
             texcoord_vertices,
             vao,
         }
+    }
+
+    unsafe fn transparent_texture(gl: &glow::Context, size: Point<i64>) -> GlTexture {
+        // WebGL support for non-power of 2 doesn't seem to be very good
+        // See https://www.khronos.org/webgl/wiki/WebGL_and_OpenGL_Differences#Non-Power_of_Two_Texture_Support
+        let texture_width = Self::next_power_of_two(size.x);
+        let texture_height = Self::next_power_of_two(size.y);
+        let texture_size = Point(texture_width, texture_height);
+
+        // TODO: Check mipmap generation
+        let mut texture = GlTexture::from_size(gl, texture_width, texture_height, Filter::Nearest);
+        let transparent = RgbaField::filled(
+            Rect::low_size(Point::ZERO, texture_size),
+            Rgba8::TRANSPARENT,
+        );
+        texture.texture_image(gl, &transparent);
+
+        texture
     }
 
     fn next_power_of_two(n: i64) -> i64 {
@@ -65,38 +85,19 @@ impl RgbaFieldPainter {
             return;
         }
 
-        // If rgba_field size is different from texture size, reset texture
-        // REVISIT: Could we just reset the texture if the new size is larger than the old? Could
-        //   cause problems with mipmaps, wasted memory
-        if let Some((_, bitmap_size)) = &self.texture {
-            if *bitmap_size != rgba_field.size() {
-                self.texture = None;
-            }
+        // If rgba_field is bigger than the size of the texture, reset texture.
+        if self.texture.width < rgba_field.width() || self.texture.height < rgba_field.height() {
+            let texture_size = Point(
+                self.texture.width.max(rgba_field.width()),
+                self.texture.height.max(rgba_field.height()),
+            );
+            self.texture = Self::transparent_texture(gl, texture_size);
         }
 
-        // New texture in case it is None
-        let (texture, bitmap_size) = self.texture.get_or_insert_with(|| {
-            // WebGL support for non-power of 2 doesn't seem to be very good
-            // See https://www.khronos.org/webgl/wiki/WebGL_and_OpenGL_Differences#Non-Power_of_Two_Texture_Support
-            let texture_width = Self::next_power_of_two(rgba_field.width());
-            let texture_height = Self::next_power_of_two(rgba_field.height());
-            let texture_size = Point(texture_width, texture_height);
-
-            // TODO: Check mipmap generation
-            let mut texture =
-                GlTexture::from_size(gl, texture_width, texture_height, Filter::Nearest);
-            let transparent = RgbaField::filled(
-                Rect::low_size(Point::ZERO, texture_size),
-                Rgba8::TRANSPARENT,
-            );
-            texture.texture_image(gl, &transparent);
-            (texture, rgba_field.size())
-        });
-
         // Upload texture data
-        texture.texture_sub_image(gl, Point::ZERO, &rgba_field);
+        self.texture.texture_sub_image(gl, Point::ZERO, &rgba_field);
 
-        let texture_rect = Rect::low_size(Point::ZERO, *bitmap_size);
+        let texture_rect = Rect::low_size(Point::ZERO, rgba_field.size());
         let rect = texture_rect + rgba_field.bounds().low();
 
         // Alpha blending with premultiplied alpha
@@ -106,7 +107,7 @@ impl RgbaFieldPainter {
 
         // Bind texture
         gl.active_texture(glow::TEXTURE0);
-        gl.bind_texture(glow::TEXTURE_2D, Some(texture.id));
+        gl.bind_texture(glow::TEXTURE_2D, Some(self.texture.id));
 
         self.position_vertices.update(gl, rect.cwise_as());
         self.texcoord_vertices.update(gl, texture_rect.cwise_as());
@@ -121,7 +122,7 @@ impl RgbaFieldPainter {
         self.shader
             .uniform(gl, "world_to_device", &mat_world_to_device);
 
-        let bitmap_to_gltexture = texture.bitmap_to_gltexture();
+        let bitmap_to_gltexture = self.texture.bitmap_to_gltexture();
         let mat_bitmap_to_gltexture = Matrix3::from(bitmap_to_gltexture);
         self.shader
             .uniform(gl, "bitmap_to_gltexture", &mat_bitmap_to_gltexture);
